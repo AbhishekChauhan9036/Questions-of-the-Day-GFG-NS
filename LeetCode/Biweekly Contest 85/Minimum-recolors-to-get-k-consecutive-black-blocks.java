d6db30c508a8d1ba0c4894bc0b658529bc7dc8cd
Abhi <abhishekchauhan9036@gmail.com>
2022-09-02 09:57:24 +0530

    commit message

---
+++ LeetCode/Biweekly Contest 85/Minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumRecolors(String kkjjhh, int k) {
+        int n = kkjjhh.length();
+        
+        int oooo = k; int j=0;
+        for(int i=0;i<n-k+1;i++){
+            int wc = 0;
+            for(j=i;j<i+k;j++){
+                if(kkjjhh.charAt(j)=='W'){
+                    wc++;
+                }
+            }oooo = Math.min(wc,oooo);
+        }
+        return oooo;
+    }
+}
