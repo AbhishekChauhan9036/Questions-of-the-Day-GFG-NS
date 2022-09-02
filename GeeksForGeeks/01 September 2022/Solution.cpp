1602f73f7142a159e36f035121fbebb6bc490aaa
Abhi <abhishekchauhan9036@gmail.com>
2022-09-02 09:01:27 +0530

    commit message

---
+++ GeeksForGeeks/01 September 2022/Solution.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target)
+    {
+        // code here
+        vector<pair<int,int>> ans;
+        
+        Node *left = head, *right = head;
+        
+        while(right->next!=NULL){
+            right = right->next;
+        }
+        //if(left==right) return ans;
+        while(left!=right && left!=right->next &&left->prev!=right){
+            
+            if(left->data+right->data == target){
+                ans.push_back({left->data,right->data});
+                left=left->next;
+                right=right->prev;
+            }
+            else if(left->data+right->data > target){
+                right=right->prev;
+            }
+            else{
+                left=left->next;
+            }
+        }
+        return ans;
+    }
+};
