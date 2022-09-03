016d1ee9c083b512d76ab0a2c69349048bb78385
Abhi <abhishekchauhan9036@gmail.com>
2022-09-03 07:46:53 +0530

    commit message

---
+++ CodeForces/Codeforces Round #818 (Div. 2)/A.cpp
@@ -0,0 +1,16 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        cout<<(n+n/2*2 +n/3*2)<<endl;
+    }
+    return 0;
+}
