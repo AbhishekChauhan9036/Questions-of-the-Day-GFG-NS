3c908076f421b0f7f417d6aa2f03088544352a17
Abhi <abhishekchauhan9036@gmail.com>
2022-09-05 20:07:58 +0530

    commit message

---
+++ GeeksForGeeks/05 September 2022/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution{
+  public:
+  int smallestSumSubarray(vector<int>& a){
+    int sum=0;
+     int mini=a[0];
+     for(int i=0;i<a.size();i++){
+         mini=min(mini,a[i]);
+         sum+=a[i];
+         mini=min(mini,sum);
+         a[i]=sum;
+     }
+     int maxi=a[0];
+     for(int i=1;i<a.size();i++){
+         maxi=max(maxi,a[i-1]);
+         
+         mini=min(a[i]-maxi,mini);
+     }
+     return mini;
+  }
+};
