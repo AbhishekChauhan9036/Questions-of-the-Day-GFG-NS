c244c920a4d3874bfa02b99a69b328c85532cd38
Abhi <abhishekchauhan9036@gmail.com>
2022-09-13 19:29:00 +0530

    commit message

---
+++ GeeksForGeeks/13 September 2022/Solution.cpp
@@ -0,0 +1,25 @@
+
+class Solution{
+  public:
+    vector<int> v;
+    void inorder(Node * root) {
+        if(!root)   return;
+
+        inorder(root->left);
+        v.push_back(root->data);
+        inorder(root->right);
+    }
+    void Convert(Node * root) {
+        if(!root)   return;
+
+        root->data = v.back();  v.pop_back();
+
+        Convert(root->right);
+        Convert(root->left);
+    }
+    void convertToMaxHeapUtil(Node* root) {
+        inorder(root);
+        Convert(root);
+    }
+
+};
