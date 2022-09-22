0a2a740c9fe73011c2e842ed98be12452c455265
Abhi <abhishekchauhan9036@gmail.com>
2022-09-22 18:27:06 +0530

    commit message

---
+++ GeeksForGeeks/22 September 2022/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+        deque<int> dq;
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(!dq.empty() && dq.front()==(i-k)) dq.pop_front();
+            while(!dq.empty() && arr[dq.back()]<arr[i]) dq.pop_back();
+            dq.push_back(i);
+            if(i>=k-1) ans.push_back(arr[dq.front()]);}
+        return ans;
+    }
+};
