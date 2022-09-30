38dc98e044eaa45966983cbd74e898899fe9bd5e
Abhi <abhishekchauhan9036@gmail.com>
2022-09-30 08:47:16 +0530

    Done

---
+++ GeeksForGeeks/28 September 2022/Solution.cpp
@@ -0,0 +1,9 @@
+class Solution{
+public:
+    bool isFit(int arr[], int brr[], int n){
+       sort(arr,arr+n);
+       sort(brr,brr+n);
+       for(int i=0;i<n;i++){
+           if(arr[i]>brr[i])return false;}
+       return true;
+    }};
