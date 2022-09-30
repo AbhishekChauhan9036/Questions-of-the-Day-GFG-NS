f3b017ef033a827bcd791f831a96306c4ce4d279
Abhi <abhishekchauhan9036@gmail.com>
2022-09-30 08:53:09 +0530

    Done

---
+++ GeeksForGeeks/30 September 2022/Solution.cpp
@@ -0,0 +1,41 @@
+int safePos(int n, int k) {
+
+        // code here
+
+            list<int> l;
+
+            for(int i=1;i<=n;i++) {
+
+              l.push_back(i);
+
+            }
+
+            auto it = l.begin();
+
+            while(l.size()>1) {
+
+               for(int i=1;i<k;i++) {
+
+                   it++;
+
+                   if(it==l.end()) {
+
+                     it = l.begin();
+
+                   }
+
+            }
+
+               it = l.erase(it);
+
+               if(it==l.end()) {
+
+               it=l.begin();
+
+            }
+
+        }
+
+         return *l.begin();
+
+    }
