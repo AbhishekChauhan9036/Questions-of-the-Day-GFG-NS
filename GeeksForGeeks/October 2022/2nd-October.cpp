6cc83c5873baddfee229ebb3f7aa0ce539e76617
Abhi <abhishekchauhan9036@gmail.com>
2022-10-03 10:25:12 +0530

    Done

---
+++ GeeksForGeeks/October 2022/2nd-October.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minChar(string str){
+        int n = str.length();
+        int i = 0 ; 
+        int j = n-1;
+        int j2 = j;
+        int count = 0;
+        while(i < j){
+            if(str[i] == str[j]){
+                i++;
+                j--;
+            }
+            else{
+                count++;
+                i = 0 ;
+                j2--;
+                j = j2;
+                
+            }
+        }
+        return count;
+    }
+};
+ 
