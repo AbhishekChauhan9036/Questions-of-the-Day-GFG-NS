6dbc3010df357b4ea710f84e740119888a0a67d5
Abhi <abhishekchauhan9036@gmail.com>
2022-10-18 07:09:07 +0530

    Done

---
+++ GeeksForGeeks/October 2022/18th-October.java
@@ -0,0 +1,29 @@
+
+class Solution{
+    static ArrayList<Integer> downwardDigonal(int N, int A[][])
+    {
+        ArrayList<Integer> a=new ArrayList<>();
+        int j=0;
+        int i=0;
+        while((i<N)&&(j<N)){
+            if((i==0)&&(j!=N-1)){
+                int k=j;
+                int l=i;
+                while(l<=j){
+                    a.add(A[l++][k--]);
+                }
+                j++;
+            }
+            else if(j==N-1){
+                int k=j;
+                int l=i;
+                while(i<=k){
+                    a.add(A[l++][k--]);
+                }
+                i++;
+            }
+        }
+        return a;
+        // code here 
+    }
+}
