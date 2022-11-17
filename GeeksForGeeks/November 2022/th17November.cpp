e4f2bfd44ea7fdeee19586e7297db62dc3b19efe
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-11-17 06:38:41 +0530

    Done

---
+++ GeeksForGeeks/November 2022/th17November.cpp
@@ -0,0 +1,20 @@
+
+class Solution{
+public:
+	// #define ll long long
+
+	long long countSubarray(int arr[], int n, int k) {
+	    int left = 0;
+	    long long ans = 0;
+	    for (int i = 0 ; i < n ; i++){
+	        for (int j = left ; j < n ; j++){
+	            if (arr[j] > k) {
+	                ans += (n - j);
+	                break;
+	            }
+	        }
+	        left++;
+	    }
+	    return ans;
+	}
+};
