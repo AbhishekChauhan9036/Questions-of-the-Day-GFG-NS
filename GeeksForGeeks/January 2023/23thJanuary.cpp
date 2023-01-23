04d1d1b0372b68e4e172a80dd1d15b00692ed8b1
abhishek <abhishek@linux.kali>
2023-01-23 07:20:47 +0530

    Done

---
+++ GeeksForGeeks/January 2023/23thJanuary.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+  public:
+     string removePair(string s) {
+      stack<char>st;
+      for(int i=0;i<s.size();i++){
+          if(st.size()>0 &&st.top()==s[i]){
+              st.pop();
+          }else st.push(s[i]);
+      }
+      s="";
+    while(!st.empty()){
+        s=s+st.top();
+        st.pop();
+    }
+    reverse(s.begin(),s.end());
+    return s.size()>0? s:"-1";
+    }
+};
