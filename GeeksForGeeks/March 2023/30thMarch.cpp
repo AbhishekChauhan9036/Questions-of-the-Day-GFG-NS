0c4c5edd7278f5fcdadfa7389db0328398c4f58d
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-03-30 06:20:23 +0530

    LC & GFG

---
+++ GeeksForGeeks/March 2023/30thMarch.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int minimumInteger(int N, vector<int> &A) {
+         sort(A.begin(),A.end());
+        long long int sum=0;
+         for(auto it:A){
+             sum+=it;
+        }
+        int ans=INT_MAX;
+    for(int i=0;i<N;i++)
+        {
+            if((long long)N*A[i]>=sum)
+            {
+               ans=min(ans,A[i]);  
+            }
+        }
+        
+        return ans;
+    }
+};
