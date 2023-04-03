38a8df0fc9ebe5b7d5038092a8c1391101105279
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-04-03 05:44:10 +0530

    LC & GFG

---
+++ GeeksForGeeks/April 2023/3rdApril.js
@@ -0,0 +1,18 @@
+class Solution
+{
+
+public:
+   int xmod11(string x)
+    {
+       // code here
+      int n = x.size();
+      int sum = 0;
+      for(int i = 0; i<n;i++){
+          sum = (sum * 10 + (x[i] - '0')) % 11;
+          
+      }
+      return sum;
+    }
+
+ 
+};
