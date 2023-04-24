d9f13ffb6c76470dde3d0f506fec39d62082ab18
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-04-24 06:04:10 +0530

    LC & GFG & NS

---
+++ NewtonSchool/Bob-and-single-digit.js
@@ -0,0 +1,45 @@
+/*--------------------------------------------------------------------------------------------------------------------------*/
+/* Author = Chauhan Abhishek */
+/* Codechef Id = https://www.codechef.com/users/abhishek_9036 */
+/* LeetCode Id = https://leetcode.com/abhishekchauhan9036/ */
+/* GeeksForGeeks Id /*= https://auth.geeksforgeeks.org/user/abhishekchauhan9036/profile */
+/* GitHub Id = https://github.com/AbhishekChauhan9036 */
+/* Language = Node.Js */
+/* Address = Ballia,UP,INDIA (221716)*/
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+
+function solution(num) {
+    if (num == 0) return 0
+    if (num % 9 == 0) return 9
+    return num % 9
+}
+
+
+function main() {
+    var t = 1;
+    // t = parseInt(readLine());
+    while (t--) {
+        let [a] = readLine().split(' ').map(Number);
+        var res = solution(a);
+        console.log(res);
+    }
+}
