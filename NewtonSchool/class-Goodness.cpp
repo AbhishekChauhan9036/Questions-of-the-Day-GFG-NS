a1e4599b77f26ed4ad45ded3bda824baddd0aff9
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-05-19 05:35:40 +0530

    LC NS & GFG

---
+++ NewtonSchool/class-Goodness.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int a,b;
+    cin>>a>>b;
+    cout<<(a|b);
+    return 0;
+}
