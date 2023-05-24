b731741277371961216bd1a5c6f0def6900f6242
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-05-24 05:41:14 +0530

    LC GFG & NS

---
+++ NewtonSchool/Game-On.cpp
@@ -0,0 +1,164 @@
+/*--------------------------------------------------------------------------------------------------------------------------*/
+/* Author = Chauhan Abhishek */
+/* Codechef Id = https://www.codechef.com/users/abhishek_9036 */
+/* LeetCode Id = https://leetcode.com/abhishekchauhan9036/ */
+/* GeeksForGeeks Id = https://auth.geeksforgeeks.org/user/abhishekchauhan9036/profile */
+/* GitHub Id = https://github.com/AbhishekChauhan9036 */
+/* Language = C++ */
+/* Address = Ballia,UP,INDIA (221716)*/
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+#pragma GCC optimize("O3,unroll-loops")
+#include<bits/stdc++.h>
+#define MOD1 998244353
+const int INF = 1e9;
+#define MOD 1000000007
+#define ll long long
+#define lli long long int
+#define lld long long double
+#define cline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define fr(a,b) for(int i = a; i < b; i++)
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define inf (1LL<<60)
+#define all(x) (x).begin(), (x).end()
+#define prDouble(x) cout << fixed << setprecision(10) << x
+#define triplet pair<ll,pair<ll,ll>>
+#define goog(tno) cout << "Case #" << tno <<": "
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define read(x) int x; cin >> x
+using namespace std;
+
+void Ramayan__Ramayan(){
+    fast_io;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+    #endif 
+}
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 10; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return mod_add(arr[0], 0, b);} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+bool revsort(ll a, ll b) {return a > b;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N)) 
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+
+bool isCellAvailable(const vector<vector<int>>& grid, int row, int col) {
+    int num_rows = grid.size();
+    int num_cols = grid[0].size();
+
+    for (int i = 0; i < num_rows; i++) {
+        if (grid[i][col] == 1)
+            return false;
+    }
+
+    for (int j = 0; j < num_cols; j++) {
+        if (grid[row][j] == 1)
+            return false;
+    }
+    return true;
+}
+
+
+int minimax(vector<vector<int>>& grid, int player, int alpha, int beta) {
+    int num_rows = grid.size();
+    int num_cols = grid[0].size();
+    bool canMove = false;
+    for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < num_cols; j++) {
+            if (grid[i][j] == 0 && isCellAvailable(grid, i, j)) {
+                canMove = true;
+                break;
+            }
+        }
+        if (canMove) break;
+    }
+
+    if (!canMove) {
+        return (player == 0) ? INF : -INF;
+    }
+
+    if (player == 0) {
+        int maxScore = -INF;
+        for (int i = 0; i < num_rows; i++) {
+            for (int j = 0; j < num_cols; j++) {
+                if (grid[i][j] == 0 && isCellAvailable(grid, i, j)) {
+                    grid[i][j] = 1;
+                    maxScore = max(maxScore, minimax(grid, 1, alpha, beta));
+                    grid[i][j] = 0;
+
+                    alpha = max(alpha, maxScore);
+                    if (alpha >= beta) return maxScore;
+                }
+            }
+        }
+        return maxScore;
+    } else {
+        int minScore = INF;
+        for (int i = 0; i < num_rows; i++) {
+            for (int j = 0; j < num_cols; j++) {
+                if (grid[i][j] == 0 && isCellAvailable(grid, i, j)) {
+                    grid[i][j] = 1;
+                    minScore = min(minScore, minimax(grid, 0, alpha, beta));
+                    grid[i][j] = 0;
+
+                    beta = min(beta, minScore);
+                    if (alpha >= beta) return minScore;
+                }
+            }
+        }
+        return minScore;
+    }
+}
+
+
+string predictWinner(vector<vector<int>>& grid) {
+    int score = minimax(grid, 0, -INF, INF);
+    return (score < 0) ? "Alice" : "Bob";
+}
+
+
+void Bhagwat_Geeta(){
+    int t=1;
+    // cin>>t;
+    while(t--)
+    {
+    	    int num_rows, num_cols;
+            cin >> num_rows >> num_cols;
+            vector<vector<int>> grid(num_rows, vector<int>(num_cols));
+            for (int i = 0; i < num_rows; i++) {
+                for (int j = 0; j < num_cols; j++) {
+                    cin >> grid[i][j];
+                }
+            }
+            string winner = predictWinner(grid);
+            cout << winner << endl;
+    }
+}
+
+signed main() {
+    // Ramayan__Ramayan();
+    Bhagwat_Geeta();
+   return 0;
+}
