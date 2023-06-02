d0f43f6da8fa625c56985d7e466ac0c7009ac65c
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-06-02 05:41:50 +0530

    LC GFG & NS

---
+++ NewtonSchool/Jumping-Stairs.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+const int MOD = 1000000007;
+int calculateNumberOfWays(int numSteps, int maxJump) {
+    vector<int> dp(numSteps + 1, 0);
+    dp[1] = 1;
+
+    for (int i = 1; i <= numSteps; ++i) {
+        for (int j = 1; j <= maxJump && i + j <= numSteps; ++j) {
+            dp[i + j] = (dp[i + j] + dp[i]) % MOD;
+        }
+    }
+    return dp[numSteps];
+}
+
+int main() {
+    int numSteps, maxJump;
+    cin >> numSteps >> maxJump;
+    int ways = calculateNumberOfWays(numSteps, maxJump);
+    cout << ways;
+    return 0;
+}
