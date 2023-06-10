f76b2d637b9d04051fbcd76ca8001c422be21879
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-06-11 05:26:03 +0530

    Question Of the Day GFG & NS

--- GeeksForGeeks/June 2023/11thJune.cpp
+++ GeeksForGeeks/June 2023/11thJune.cpp
@@ -1,34 +1,18 @@
 
-class Solution
-{
+
+
+
+class Solution{
     public:
-         void Rearrange(int arr[], int n)
-        {
-            // Your code goes here
-            vector<int> v;
-            stack<int> a;
-            stack<int> b;
-            stack<int> c;
-            for(int i=0;i<n;i++){
-                if(arr[i]<0) a.push(arr[i]);
-                else b.push(arr[i]);
-            }
-            while(!a.empty()) {
-                v.push_back(a.top());
-                a.pop();
-                
-            }
-            reverse(v.begin(),v.end());
-            while(!b.empty()) {
-                c.push(b.top());
-                b.pop();
-                
-            }
-            while(!c.empty()) {
-                v.push_back(c.top());
-                c.pop();
-            }
-            for(int i=0;i<n;i++) arr[i]=v[i];
-        }
+    void update(int a[], int n, int updates[], int k)
+    {
+       for(int i=0;i<k;i++){
+           a[updates[i]-1]+=1;
+       }
+       for(int i=1;i<n;i++){
+           a[i]+=a[i-1];
+       }
+    }
 };
+
 
