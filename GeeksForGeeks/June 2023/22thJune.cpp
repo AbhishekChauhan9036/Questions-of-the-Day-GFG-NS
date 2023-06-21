1774f80ef9740794b98b727f86b3108edc6abc66
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-06-22 05:09:23 +0530

    Question Of the Day GFG & NS

--- GeeksForGeeks/June 2023/22thJune.cpp
+++ GeeksForGeeks/June 2023/22thJune.cpp
@@ -1,10 +1,37 @@
 
 class Solution {
-   public:
-    const long long mod = 1e9+7;
-    int sumOfNaturals(int n) {
+  public:
+    bool lemonadeChange(int N, vector<int> &bills) {
         // code here
-        return ((n%mod*(n+1)%mod)%mod/2)%mod;
+        int countFive = 0, countTen = 0, countTwenty = 0;
+
+    // Check if the first customer pays with a $10 or $20 bill, which cannot be given change
+    if (bills[0] == 10 || bills[0] == 20)
+        return false;
+
+    for (int i = 0; i < N; i++) {
+        if (bills[i] == 5) {
+            countFive++;  // Increase count of $5 bills
+        } else if (bills[i] == 10) {
+            if (countFive > 0) {
+                countFive--;  // Use a $5 bill to give change for $10
+                countTen++;   // Increase count of $10 bills
+            } else {
+                return false;  // Unable to give change for $10
+            }
+        } else if (bills[i] == 20) {
+            if (countFive > 0 && countTen > 0) {
+                countTen--;   // Use a $10 bill
+                countFive--;  // Use a $5 bill
+            } else if (countFive > 2 && countTen == 0) {
+                for (int j = 0; j < 3; j++)
+                    countFive--;  // Use three $5 bills
+            } else {
+                return false;  // Unable to give change for $20
+            }
+        }
+    }
+
+    return true;  // All customers were able to receive correct change
     }
 };
-
