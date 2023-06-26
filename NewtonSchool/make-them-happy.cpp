44423eb21d85e23297d5a89e8e2673862e01becf
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-06-26 05:37:02 +0530

    Question Of the Day GFG & NS

---
+++ NewtonSchool/make-them-happy.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+    int k1, k2, k3;
+    cin >> k1 >> k2 >> k3;
+    cout<<((k1 <= k3 && k2 <= k3)?"yes":"no");
+    return 0;
+}
