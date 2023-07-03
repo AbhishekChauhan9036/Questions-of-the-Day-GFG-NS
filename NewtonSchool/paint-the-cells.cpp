f2d104cb96efbdc9a9be852df02706c59f6e4f3d
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-07-03 05:37:07 +0530

    Question Of the Day GFG & NS

---
+++ NewtonSchool/paint-the-cells.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+#define ll long long
+int main() {
+	// Your code here
+    ll r1;
+    cin>>r1;
+    cout<<((r1%2!=0)?"Alice":"Bob");
+    return 0;
+}
