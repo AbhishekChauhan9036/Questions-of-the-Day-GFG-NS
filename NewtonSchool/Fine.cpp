7a2faa760000fdedb71fc8f9ed7b3ddbfa2fa835
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-07-04 05:38:28 +0530

    Question Of the Day GFG & NS

---
+++ NewtonSchool/Fine.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+    int r1, r2, r3;
+    cin >> r1 >> r2 >> r3;
+    cout<<((r1 - r2)* r3);
+    return 0;
+}
