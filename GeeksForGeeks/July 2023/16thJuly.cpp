16004e4cec2dc93938ea706382928db151c92dff
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-07-16 05:50:44 +0530

    Question Of the Day GFG & NS

---
+++ GeeksForGeeks/July 2023/16thJuly.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q){
+        stack<int> st;
+        while(!q.empty()){
+           st.push(q.front());
+           q.pop();
+        }
+        while(!st.empty()){
+            q.push(st.top());
+            st.pop();
+        }
+        return q;
+    }
+};
