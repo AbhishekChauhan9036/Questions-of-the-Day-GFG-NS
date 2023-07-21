2952795af4a3fa9ec1a4e7c7a4d078c825a995f6
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-07-21 08:21:36 +0530

    Question Of the Day GFG & NS

---
+++ NewtonSchool/Fare-Ride.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+    int k1, k2;
+    cin >> k1 >> k2;
+
+    bool k3 = (k1 >= k2);
+    cout << boolalpha << k3 << endl;
+
+    return 0;
+}
