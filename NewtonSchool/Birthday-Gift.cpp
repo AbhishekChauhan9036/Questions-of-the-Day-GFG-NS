b287909bf5773fc1902aeba790dcd42b4e5f6271
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-07-26 07:05:30 +0530

    Question Of the Day GFG & NS

---
+++ NewtonSchool/Birthday-Gift.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+    int k1, k2;
+    cin >> k1 >> k2;
+    cout << (k1 >= k2 ? "true" : "false") << endl;
+    return 0;
+}
