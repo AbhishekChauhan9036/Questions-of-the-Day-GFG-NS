740c95a5b13b14cca4229ae980ff77aecbe8b4b4
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-08-02 19:52:03 +0530

    Question Of the Day GFG & NS

---
+++ NewtonSchool/Candy-Robbery.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+
+    if (n >= m) {
+        std::cout << "true" << std::endl;
+    } else {
+        std::cout << "false" << std::endl;
+    }
+
+    return 0;
+}
