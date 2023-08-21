dfc97a59e8e456e846d361d72502c8b931a59854
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-08-21 11:58:15 +0530

    GFG & NS

---
+++ NewtonSchool/Nth-Number-in-the-List.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+    // Your code here
+    int n;
+    cin >> n;
+    cout<<static_cast<int>(pow(n, 3) - pow(n, 2));
+    return 0;
+}
