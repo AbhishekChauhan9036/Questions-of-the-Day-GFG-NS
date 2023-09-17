85e4d0e3f584b667941a2d9b86933573901171d5
chauhan9036 <chauhanabhishek@sharpener.tech>
2023-09-16 20:04:01 -0800

    GFGPOTD

---
+++ GeeksForGeeks/September 2023/17thSeptember.cpp
@@ -0,0 +1,13 @@
+
+
+
+class Solution
+{
+    public:
+    vector<long long> printFibb(int n) {
+        vector<long long> dp(n);
+        dp[0]=1; dp[1]=1;
+        for(int i=2;i<n;i++) dp[i]=dp[i-1]+dp[i-2];
+        return dp;
+    }
+};
